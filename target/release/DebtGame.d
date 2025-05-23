9de5568f7d436154bb2acdfb5a5e40347f202496
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-05-22 23:04:43 -0700

    twttr setup ✅

---
+++ target/release/DebtGame.d
@@ -0,0 +1,1 @@
+/Users/Admin/stimstation/stimstation/target/release/DebtGame: /Users/Admin/stimstation/stimstation/src/main.rs
---
+++ target/release/deps/DebtGame-3346be12cf7605ff.d
@@ -0,0 +1,5 @@
+/Users/Admin/stimstation/stimstation/target/release/deps/DebtGame-3346be12cf7605ff: src/main.rs
+
+/Users/Admin/stimstation/stimstation/target/release/deps/DebtGame-3346be12cf7605ff.d: src/main.rs
+
+src/main.rs:

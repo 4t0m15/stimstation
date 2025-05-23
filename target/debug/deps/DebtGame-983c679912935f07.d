c873e4ad9e6712e2c0e52cd50598e0637dc80b3e
Arsen Martirosyan <144264687+4t0m15@users.noreply.github.com>
2025-05-22 23:02:57 -0700

    setup files for stuff to be made

---
+++ target/debug/deps/DebtGame-983c679912935f07.d
@@ -0,0 +1,5 @@
+/Users/Admin/stimstation/stimstation/target/debug/deps/libDebtGame-983c679912935f07.rmeta: src/main.rs
+
+/Users/Admin/stimstation/stimstation/target/debug/deps/DebtGame-983c679912935f07.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/DebtGame-b58147cc37714293.d
@@ -0,0 +1,5 @@
+/Users/Admin/stimstation/stimstation/target/debug/deps/libDebtGame-b58147cc37714293.rmeta: src/main.rs
+
+/Users/Admin/stimstation/stimstation/target/debug/deps/DebtGame-b58147cc37714293.d: src/main.rs
+
+src/main.rs:
